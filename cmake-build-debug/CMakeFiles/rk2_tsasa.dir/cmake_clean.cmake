a684a2237ea61cda64f4e2030a584ccc8f343ece
zhenerenya <zhenyachmchk@gmail.com>
2020-11-11 17:40:35 +0300

    create project

---
+++ cmake-build-debug/CMakeFiles/rk2_tsasa.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rk2_tsasa.dir/main.cpp.o"
+  "rk2_tsasa"
+  "rk2_tsasa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rk2_tsasa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/rk2_tsasa.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for rk2_tsasa.
+# This may be replaced when dependencies are built.
